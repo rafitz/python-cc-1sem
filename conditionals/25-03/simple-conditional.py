116e55ed55f553511b8ff4a7e7b7cfa2d12cf331
Rafitz <rafaelfocaca@gmail.com>
2025-03-25 10:39:58 -0300

    feet: simple conditional

---
+++ conditionals/25-03/simple-conditional.py
@@ -0,0 +1,7 @@
+a = float(input("Digite o valor de A:"))
+b = float(input("Digite o valor de B:"))
+
+if a > b:
+    print(f"{a:.1f} é Maior que {b:.1f}")
+else:
+    print(f"{a:.1f} é Menor que {b:.1f}")
